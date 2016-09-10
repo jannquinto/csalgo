ab8d1fdee84a3f3c5882fd1b65b299ed3f7079da
JANN ADRIAN QUINTO <JCQUINTO@fit.edu.ph>
2016-09-10 20:44:24 +0800

    Test string

--- LexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
+++ LexicographicGenerator/src/lexicographicgenerator/LexicographicGenerator.java
@@ -1,5 +1,9 @@
+import java.util.*;
 public class LexicographicGenerator {
     public static void main(String[] args) {
-        
+        String charSet = "ABC";
+        for(int i = 0; i < charSet.length(); i++){
+            System.out.println(charSet.charAt(i));
+        }
     } 
 }
